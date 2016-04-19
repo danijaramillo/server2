8fc547718e972989b6e1c2cd2820e249668cc8ec
Daniel Jaramillo <danieljaramillo3@gmail.com>
2016-04-19 19:32:18 -0400

    Server being pushed

--- server.rb
+++ server.rb
@@ -1,15 +1,18 @@
 require 'sinatra'
 
+# get '/' do
+#   "Hello World"
+# end
+
 get '/' do
-  "Hello World"
+  send_file 'public/hello.html'
 end
 
-
-get '/sinatra' do
-  "Hello Sinatra"
-end
-
-
-get '/error' do
-  raise "boom"
-end
+# get '/sinatra' do
+#   "Hello Sinatra"
+# end
+#
+#
+# get '/error' do
+#   raise "boom"
+# end
